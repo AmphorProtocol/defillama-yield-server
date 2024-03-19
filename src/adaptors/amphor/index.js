64eab7d08b57ae2c5b8f81fc06c208094042cb1e
blablalf <contact@alfred-gaillard.fr>
2024-03-19 16:19:57 +0100

    getting ready, just need to update the PT apr and apy values

--- src/adaptors/amphor/index.js
+++ src/adaptors/amphor/index.js
@@ -5,11 +5,17 @@ const poolsFunction = async () => {
 
   const wbtc = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599";
   const wsteth = "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0";
-  const prices = (await utils.getPrices([wsteth, wbtc], 'ethereum'))
+  const PTezETH = "0xeEE8aED1957ca1545a0508AfB51b53cCA7e3c0d1";
+  const PTrsETH = "0xB05cABCd99cf9a73b19805edefC5f67CA5d1895E";
+  const PTweETH = "0xc69Ad9baB1dEE23F4605a82b3354F8E40d1E5966";
+  const prices = (await utils.getPrices([wsteth, wbtc, PTezETH, PTrsETH, PTweETH], 'ethereum'))
     .pricesByAddress;
   const usdcVaultAddress = '0x3b022EdECD65b63288704a6fa33A8B9185b5096b';
   const wstethVaultAddress = '0x2791EB5807D69Fe10C02eED6B4DC12baC0701744';
   const wbtcVaultAddress = '0xC4A324fDF8a2495776B4d6cA46599B5a52f96489';
+  const PTezETHVaultAddress = '0x920F17e741029D904936c58a545DFFC72f82C079';
+  const PTrsETHVaultAddress = '0x0498b85FB4EC85EF5EFe82513aa9DaF767358A15';
+  const PTweETHVaultAddress = '0xf97ecda5F9ff31d83f635a6EA70D2D3B9C8f2e00';
 
   const ERC4626TotalAssets =
     {
@@ -39,6 +45,24 @@ const poolsFunction = async () => {
     'https://app.amphor.io/api/apr?vault=wbtcVault'
   );
   const wbtcApy = ((1 + Number(JSON.stringify(wbtcAprData.wbtcVault.strategyNetAPR))/2600) ** (26) - 1) * 100;
+  
+  //todo
+  const PTezETHAprData = await utils.getData(
+    'https://app.amphor.io/api/apr?vault=wbtcVault'
+  );
+  const PTezETHApy = ((1 + Number(JSON.stringify(wbtcAprData.wbtcVault.strategyNetAPR))/2600) ** (26) - 1) * 100;
+
+  //todo
+  const PTrsETHAprData = await utils.getData(
+    'https://app.amphor.io/api/apr?vault=wbtcVault'
+  );
+  const PTrsETHApy = ((1 + Number(JSON.stringify(wbtcAprData.wbtcVault.strategyNetAPR))/2600) ** (26) - 1) * 100;
+  
+  //todo
+  const PTweETHAprData = await utils.getData(
+    'https://app.amphor.io/api/apr?vault=wbtcVault'
+  );
+  const PTweETHApy = ((1 + Number(JSON.stringify(wbtcAprData.wbtcVault.strategyNetAPR))/2600) ** (26) - 1) * 100;
 
   const usdcTotalAsset = await sdk.api.abi.call({
     abi: ERC4626TotalAssets,
@@ -54,6 +78,21 @@ const poolsFunction = async () => {
     abi: ERC4626TotalAssets,
     chain: 'ethereum',
     target: wbtcVaultAddress,
+  });
+  const PTezETHTotalAsset = await sdk.api.abi.call({
+    abi: ERC4626TotalAssets,
+    chain: 'ethereum',
+    target: PTezETHVaultAddress,
+  });
+  const PTrsETHTotalAsset = await sdk.api.abi.call({
+    abi: ERC4626TotalAssets,
+    chain: 'ethereum',
+    target: PTrsETHVaultAddress,
+  });
+  const PTweETHTotalAsset = await sdk.api.abi.call({
+    abi: ERC4626TotalAssets,
+    chain: 'ethereum',
+    target: PTweETHVaultAddress,
   });
 
   const usdcPool = {
@@ -83,7 +122,34 @@ const poolsFunction = async () => {
     apy: wbtcApy,
   };
 
-  return [usdcPool, wstethPool, wbtcPool];
+  const PTezETHPool = {
+    pool: PTezETHVaultAddress,
+    chain: 'ethereum',
+    project: 'amphor',
+    symbol: utils.formatSymbol('PTezETH'),
+    tvlUsd: (Number(PTezETHTotalAsset.output)/1e18) * prices[PTezETH.toLowerCase()],
+    apy: PTezETHApy,
+  };
+
+  const PTrsETHPool = {
+    pool: PTrsETHVaultAddress,
+    chain: 'ethereum',
+    project: 'amphor',
+    symbol: utils.formatSymbol('PTrsETH'),
+    tvlUsd: (Number(PTrsETHTotalAsset.output)/1e18) * prices[PTrsETH.toLowerCase()],
+    apy: PTrsETHApy,
+  };
+
+  const PTweETHPool = {
+    pool: PTweETHVaultAddress,
+    chain: 'ethereum',
+    project: 'amphor',
+    symbol: utils.formatSymbol('PTweETH'),
+    tvlUsd: (Number(PTweETHTotalAsset.output)/1e18) * prices[PTweETH.toLowerCase()],
+    apy: PTweETHApy,
+  };
+
+  return [usdcPool, wstethPool, wbtcPool, PTezETHPool, PTrsETHPool, PTweETHPool];
 };
 
 module.exports = {
